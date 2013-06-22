f2054de478f094f0f4a7ff5736353bdf45598556
Carlos Cordoba <ccordoba12@gmail.com>
2013-06-22 16:05:28 -0500

    debug_loop.py: Add an intermediate variable to easily check how data_out values are changing

--- debug_demo/debug_loop.py
+++ debug_demo/debug_loop.py
@@ -6,4 +6,5 @@ Created on Wed Jun 19 21:13:10 2013
 
 for idx in xrange(1, len(data_in)):
     # Insert data processing here
-    data_out[idx] = data_in[idx - 1] + data_in[idx]
+    new_data = data_in[idx - 1] + data_in[idx]
+    data_out[idx] = new_data
